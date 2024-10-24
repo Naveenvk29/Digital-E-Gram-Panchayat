9a1bac1a6dce2cd2a8aff61381c073f56a06ed42
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-10-24 13:45:08 +0530

    add cors

--- server/app.js
+++ server/app.js
@@ -10,11 +10,12 @@ const app = express();
 
 // app.use(cors({ origin: "http://localhost:5173" }));
 
-app.use(
-  cors({
-    origin: "https://digital-e-gram-panchayat.vercel.app",
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "https://digital-e-gram-panchayat.vercel.app",
+//   })
+// );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

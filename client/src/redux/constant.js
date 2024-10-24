f4fe598e2c817c8bdaaeadff366a4d9073eed2a1
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-10-24 16:24:02 +0530

    test

--- client/src/redux/constant.js
+++ client/src/redux/constant.js
@@ -1,4 +1,7 @@
 export const BASE_URL = "https://digital-e-gram-panchayat-api.onrender.com";
-export const USER_URL = "/api/users";
-export const SERVICES_URL = "/api/services";
-export const APPLICATION_URL = "/api/applications";
+export const USER_URL =
+  "https://digital-e-gram-panchayat-api.onrender.com/api/users";
+export const SERVICES_URL =
+  "https://digital-e-gram-panchayat-api.onrender.com/api/services";
+export const APPLICATION_URL =
+  "https://digital-e-gram-panchayat-api.onrender.com/api/applications";
